09cbe561b336430dde80d23bcb4e4f6749f376b1
Tanisha Ghosh <ghoshatanisha@gmail.com>
2026-08-15 10:07:40 +0530

    grade changes

--- grade.py
+++ grade.py
@@ -1,6 +1,6 @@
 score = int(input("Enter your score: "))
 
-if score >=90 and score<=100:
+if 90 <= score <=100:
     print("Your grade is A")
 elif score>=80 and score<=90:
     print("Your grade is B")

43526a3098f5b00d0eb783ac1c67bfdffc9d1c75
Tanisha Ghosh <ghoshatanisha@gmail.com>
2026-08-15 10:42:34 +0530

    using boolean

---
+++ evenoddfunction.py
@@ -0,0 +1,15 @@
+def main():
+    x = int(input("what is the value of x? "))
+    if is_even(x):
+        print("even")
+              
+    else:
+        print("odd")
+
+def is_even(n):
+    if n%2==0:
+        return True
+    else:
+        return False
+
+main()

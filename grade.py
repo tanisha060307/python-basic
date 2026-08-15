a1c74a40f3eca53deacce760dbb016bcd9fde6d4
Tanisha Ghosh <ghoshatanisha@gmail.com>
2026-08-15 09:59:43 +0530

    grade

---
+++ grade.py
@@ -0,0 +1,13 @@
+score = int(input("Enter your score: "))
+
+if score >=90 and score<=100:
+    print("Your grade is A")
+elif score>=80 and score<=90:
+    print("Your grade is B")
+elif score>=70 and score<=80:
+    print("Your grade is C")
+elif score>=60 and score<=70:
+    print("Your grade is D")
+else:
+    print("Your grade is F")
+

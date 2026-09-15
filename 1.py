c23e1b486c38cac6efbc2ac66f709a1cbcb6540c
Tanisha Ghosh <ghoshatanisha@gmail.com>
2026-09-15 19:28:44 +0530

    swapping with 3rd variable

--- 1.py
+++ 1.py
@@ -1,13 +1,7 @@
-import math
-length = float(input("Enter the length of the rectangle: "))
-width = float(input("Enter the width of the rectangle: "))
-print("rect area=", length*width)
-print("rect perimeter=", 2*(length+width))
-
-radi = float(input("Enter the radius of the circle: "))
-print("circle area = ",math.pi*radi*radi)
-print("circle perimeter = ", 2*math.pi*radi) 
-
-side = float(input("Enter the side of the square: "))
-print("square perimeter = ", 4*side)
-print("square area = ", side*side)
+a=int(input("enter the first number:"))
+b=int(input("enter the second number:"))
+print("before swapping:",a,b)
+temp=a
+a=b
+b=temp
+print("after swapping:",a,b)
